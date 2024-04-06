b9c480ff0008d9088a329c80ce7bc23b7a4e5b95
PanCamel <wbalucki18@gmail.com>
2024-04-06 16:34:04 +0200

    -

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Matrix;
+
+Matrix2D m = new Matrix2D(2, 2, 3, 3);
+
+Console.WriteLine(m.ToString());
